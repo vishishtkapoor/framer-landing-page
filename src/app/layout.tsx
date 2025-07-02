625c0dd98d38306ae584cdc1b11c9323d43acef0
vishishtkapoor <vishishtkapoor2@gmail.com>
2025-07-02 15:53:49 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import clsx from "clsx";
+import "./globals.css";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "IOT Dashboard",
+  description: "iot dashboard for electronics and iot devices data visualization",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={clsx(dmSans.className, "antialiased")}>{children}</body>
+    </html>
+  );
+}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { AnimatedGradientTextDemo } from './animatedtext';
+
+export const Hero = () => {
+  return (
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 relative overflow-clip">
+      <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] llg:h-[800px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
+    <div className="container relative">
+      <div className="flex items-center justify-center -mt-10">
+        <AnimatedGradientTextDemo/>
+      </div>
+      <div className="flex justify-center mt-8 ">
+      <div className="inline-flex relative">
+      <h1 className='text-7xl sm:text-9xl font-bold tracking-tightner text-center inline-flex'>Get your IOT <br/>DASHBOARD</h1>      </div>
+      </div>
+      <div className="flex justify-center">
+      <p className='text-xl text-center mt-8 max-w-md'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit quod dicta, nisi autem dignissimos consequatur? Unde saepe eius nam. Minima at quaerat non quae qui consequatur hic nam neque earum.</p>
+      </div>
+      <div className="flex justify-center mt-8">
+      <button className='bg-white text-black py-3 px-5 rounded-lg font-medium'>Book a Call</button>
+      </div>
+
+
+    </div>
+    
+
+    </div>
+  )
+};
